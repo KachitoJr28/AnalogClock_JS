9665a26ee3d4a8d1554b6378a703f0f9a28527ff
KachitoJr28 <orefome009@gmail.com>
2024-09-01 23:47:57 +0100

    1.0

--- script.js
+++ script.js
@@ -1,3 +1,34 @@
-let hourHand=document.getElementById("hour")
-let minHand=document.getElementById("min")
-let secHand=document.getElementById("sec")
+
+// currentTime = `${time.getHours()}:${time.getMinutes()}:${time.getSeconds()}`
+// console.log(currentTime)
+
+
+function rotateHands(hourAngle,minAngle,secAngle) {
+    let hourHand=document.getElementById("hour")
+    hourHand.style.transform=`rotate(${hourAngle}deg)`
+
+    let minHand=document.getElementById("min")
+    minHand.style.transform=`rotate(${minAngle}deg)`
+
+    let secHand=document.getElementById("sec")
+    secHand.style.transform=`rotate(${secAngle}deg)`
+
+}
+
+function updateClock() {
+    const time = new Date();
+    const hour=time.getHours()
+    const min=time.getMinutes()
+    const sec=time.getSeconds()
+
+    const hourAngle=(hour*30)+(min*0.5)
+    const minAngle=(min*6)+(0.1*sec)
+    const secAngle=(sec*6)
+
+    rotateHands(hourAngle,minAngle,secAngle)
+}
+
+setInterval(updateClock, 1000);
+// updateClock()
+
+let digitalClock=document.getElementById("digital")
