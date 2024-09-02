8c0dcb239b4cbb0c24e79ba612ecf8298e93d4d9
KachitoJr28 <orefome009@gmail.com>
2024-09-02 12:50:24 +0100

    2.0

--- script.js
+++ script.js
@@ -1,34 +1,36 @@
-
-// currentTime = `${time.getHours()}:${time.getMinutes()}:${time.getSeconds()}`
-// console.log(currentTime)
-
-
-function rotateHands(hourAngle,minAngle,secAngle) {
-    let hourHand=document.getElementById("hour")
-    hourHand.style.transform=`rotate(${hourAngle}deg)`
-
-    let minHand=document.getElementById("min")
-    minHand.style.transform=`rotate(${minAngle}deg)`
-
-    let secHand=document.getElementById("sec")
-    secHand.style.transform=`rotate(${secAngle}deg)`
-
-}
+let hourAngle=0,minAngle=0,secAngle=0
+let hourHand=document.getElementById("hour"),
+ minHand=document.getElementById("min"),
+ secHand=document.getElementById("sec")
 
 function updateClock() {
+    const secIncrement=6/60    
+    const minIncrement=0.1/60    
+    const hourIncrement=0.0083/60    
+
+    hourAngle+=hourIncrement
+    minAngle+=minIncrement
+    secAngle+=secIncrement
+
+    hourHand.style.transform=`rotate(${hourAngle}deg)`
+    minHand.style.transform=`rotate(${minAngle}deg)`
+    secHand.style.transform=`rotate(${secAngle}deg)`
+    requestAnimationFrame(updateClock)
+}
+
+function initClock() {
     const time = new Date();
-    const hour=time.getHours()
+    const hour=time.getHours()%12
     const min=time.getMinutes()
     const sec=time.getSeconds()
 
-    const hourAngle=(hour*30)+(min*0.5)
-    const minAngle=(min*6)+(0.1*sec)
-    const secAngle=(sec*6)
+    hourAngle=(hour*30)+(min*0.5)
+    minAngle=(min*6)+(0.1*sec)
+    secAngle=(sec*6)
 
-    rotateHands(hourAngle,minAngle,secAngle)
+    requestAnimationFrame(updateClock)
 }
 
-setInterval(updateClock, 1000);
-// updateClock()
+initClock()
 
 let digitalClock=document.getElementById("digital")
